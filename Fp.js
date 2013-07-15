650a2c82ba050044f598d9b908a2eef81f0df326
flux0r <flux0r@brettmingalls.com>
2013-07-14 23:51:22 -0700

    Trying a different approach.

--- Fp.js
+++ Fp.js
@@ -1,75 +1,59 @@
-(function (UNIVERSE) {
-    "use strict";
-
-    var
-        /* Module */
-        Fp,
-
-        /* Constructors */
-        List,
-        Pair,
-
-        /* Functions */
-        mk,
-        product,
-        sum;
-
-    product = function (name, fields) {
-        var f;
-        f = function () {
-            var i, o;
-            if (!(this instanceof f)) {
-                o = Object.create(f.prototype);
-                f.apply(o, arguments);
-                return o;
-            }
-            if (arguments.length !== fields.length) {
-                throw new TypeError(name + ": Expected " + fields.length +
-                        " fields. Got " + arguments.length + ".");
-            }
-            for (i = 0; i < fields.length; i += 1) {
-                this[fields[i]] = arguments[i];
-            }
-            this.tag = name;
-        };
-        f.tag = name;
-        f.fields = fields.length;
-        return f;
+var Product = function (type, fields) {
+    var f;
+    f = function () {
+        var o;
+        if (!(this instanceof f)) {
+            o = Object.create(f.prototype);
+            f.apply(o, arguments);
+            return o;
+        }
+        if (fields.length != arguments.length) {
+            throw new TypeError(type + ": Expected " + fields.length +
+                    " but got " + arguments.length + ".")
+        }
+        for (i = 0; i < arguments.length; i += 1) {
+            this[fields[i]] = arguments[i]
+        }
     };
+    f.type = type;
+    f.length = fields.length;
+    return f;
+};
 
-    sum = function (name, cs) {
-        var f;
-        f = function () {
-            var c, o;
-            if (!(this instanceof f)) {
-                o = Object.create(f.prototype);
-                f.apply(o, arguments);
-                return o;
-            }
-            for (c in cs) {
-                if (cs.hasOwnProperty(c)) {
-                    this[c] = product(c, cs[c]);
-                    this[c].type = name;
-                }
-            }
-            this.type = name;
-            this.branches = Object.keys(cs).length;
+var Data = function (type, maker, pred, unmaker) {
+    var o = Object.create(null)
+    o.type = type;
+    o.maker = maker;
+    o.pred = pred;
+    o.unmaker = unmaker;
+    return o;
+};
+
+var recordToDataType = function () {
+    return mkDataTypeMaker(Records, rtdToDataType);
+};
+
+var DataTypeMaker = function (typeClass, f) {
+    return function (args) {
+        var data = f.apply(this, args);
+        var namer = function (p) {
+           return p(typeClass, data.type);
         };
-        return f();
+        return mkData(function (p) { return p(typeClass); });
     };
+};
 
-    Pair = product("Pair", ["car", "cdr"]);
-    List = sum("List", { Cons: ["head", "tail"], Nil: [] });
+//var DataType = function (tag, fields, options) {
+//    var o, uuid;
+//    o = Object.create(null);
+//    if (options.constructor === undefined) {
+//        o.hasAConstructor = false;
+//    } else {
+//        o.hasAConstructor = true;
+//    }
+//    o.uuid = options.uuid;
+//    o.rtd = mkDataTypeRtd(tag, fields);
+//    if (o.hasAConstructor) {
+//        recordToDataType(o.rtd, function (maker, pred, unmaker) {
 
-    Fp = Object.create(null);
 
-    Fp.List = List;
-    Fp.Pair = Pair;
-
-    Fp.mk = mk;
-    Fp.product = product;
-    Fp.sum = sum;
-
-    UNIVERSE.Fp = Fp;
-
-}(this));
