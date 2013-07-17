c3e9a0690a277aff93a70e83dc1f263a4b4ccb3b
flux0r <flux0r@brettmingalls.com>
2013-07-17 00:50:10 -0700

    Work on data constructors and environment.

--- Fp.js
+++ Fp.js
@@ -1,59 +1,119 @@
-var Product = function (type, fields) {
-    var f;
-    f = function () {
-        var o;
-        if (!(this instanceof f)) {
-            o = Object.create(f.prototype);
-            f.apply(o, arguments);
-            return o;
+(function (UNIVERSE) {
+    
+    var
+        Fp,
+        
+        environment,
+        extend,
+        isNotDefined,
+        mkData,
+        mkDataConstructor,
+        mkDataDeconstructor,
+        mkDataPredicate;
+    
+    Fp = Object.create(null);
+    
+    extend = function (x, y) {
+        var prop;
+        for (prop in y) {
+            if (y.hasOwnProperty(prop)) {
+                x[prop] = y[prop]; 
+            }
         }
-        if (fields.length != arguments.length) {
-            throw new TypeError(type + ": Expected " + fields.length +
-                    " but got " + arguments.length + ".")
-        }
-        for (i = 0; i < arguments.length; i += 1) {
-            this[fields[i]] = arguments[i]
-        }
+        return x;
     };
-    f.type = type;
-    f.length = fields.length;
-    return f;
-};
-
-var Data = function (type, maker, pred, unmaker) {
-    var o = Object.create(null)
-    o.type = type;
-    o.maker = maker;
-    o.pred = pred;
-    o.unmaker = unmaker;
-    return o;
-};
-
-var recordToDataType = function () {
-    return mkDataTypeMaker(Records, rtdToDataType);
-};
-
-var DataTypeMaker = function (typeClass, f) {
-    return function (args) {
-        var data = f.apply(this, args);
-        var namer = function (p) {
-           return p(typeClass, data.type);
+    
+    isNotDefined = function (x) {
+        return x === undefined;
+    };
+    
+    environment = function (dataConstructors) {
+        if (!(this instanceof environment)) {
+            return new environment(typeClasses, dataConstructors);
+        }
+        
+        dataConstructors = dataConstructors || {};
+        
+        
+    };
+    
+    mkDataConstructor = function (dataName, fields) {
+        maker = function () {
+            var i, o;
+            if (!(this instanceof maker)) {
+                o = Object.create(maker.prototype);
+                maker.apply(o, arguments);
+                return o;
+            }
+            if (arguments.length !== fields.length) {
+                throw new TypeError(dataName + ": Expected " + fields.length  + 
+                        " fields but got " + arguments.length + ".");
+            }
+            for (i = 0; i < fields.length; i += 1) {
+                this[fields[i]] = arguments[i];
+            }
         };
-        return mkData(function (p) { return p(typeClass); });
+        maker.tag = dataName;
+        return maker;
     };
+    
+    mkDataPredicate = function (fields) {
+        var p;
+        p = function (x) {
+            var i;
+            for (i = 0; i < fields.length; i += 1) {
+                if (isNotDefined(x[fields[i]])) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        return p;
+    };
+    
+    mkDataDeconstructor = function (fields) {
+        var d;
+        d = function (x, k) {
+            return x[k];
+        };
+        return d;
+    };
+    
+    mkData = function (dataName, fields) {
+        return {
+            meta: fields,
+            maker: mkDataConstructor(dataName, fields),
+            pred: mkDataPredicate(fields),
+            unmaker: mkDataDeconstructor(fields),
+        };
+    };
+    
+    mkType = function (type, constructors) {
+        var branches;
+    }
+
+
+/*Pair = mkRecordConstructor("Pair", ["car", "cdr"]);
+
+testPair1 = car(Pair(3, 4)) === 3;
+testPair2 = cdr(Pair(3, 4)) === 4;
+*/
+
+/*
+Maybe = mkTypeConstructor("Maybe", { Just: ["val"], Nothing: [] });
+
+fromMaybe = function (defval, x) {
+    x.match({ Just: x.val, Nothing: defval });
 };
 
-//var DataType = function (tag, fields, options) {
-//    var o, uuid;
-//    o = Object.create(null);
-//    if (options.constructor === undefined) {
-//        o.hasAConstructor = false;
-//    } else {
-//        o.hasAConstructor = true;
-//    }
-//    o.uuid = options.uuid;
-//    o.rtd = mkDataTypeRtd(tag, fields);
-//    if (o.hasAConstructor) {
-//        recordToDataType(o.rtd, function (maker, pred, unmaker) {
 
+testMaybe1 = fromMaybe(0, Just(3)) === 3;
+testMaybe2 = fromMaybe(0, Nothing()) === 0;
+*/
 
+Fp.extend = extend;
+Fp.mkData = mkData;
+
+UNIVERSE.Fp = Fp;
+
+}(this));
